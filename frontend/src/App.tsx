9a3cdd478c3503839d718c908c1081007da4c96c
Alex <probesalesman@gmail.com>
2021-05-17 15:48:53 -0400

    adds to dockerignore files, and frontend CRA app is able to run - still need to allow frontend and backend apps to communicate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface ITodo {
+  id: number;
+  description: string;
+}
+
+interface ITodoResponse {
+  data: {
+    todos: ITodo[];
+  };
+}
 
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchTodos(): Promise<void> {
+      try {
+        const todosReq = await fetch("/api/todos");
+
+        let todosRes;
+        if (todosReq.ok && todosReq.status === 200) {
+          todosRes = await todosReq.json();
+        } else {
+          throw new Error("server error");
+        }
+
+        setTodos(todosRes);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      }
+    }
+
+    fetchTodos();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,15 +46,27 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        This is just a test app to connect a front and back end with Docker.
+        Will eventually include a CI/CD pipeline
+      </main>
+      <div>
+        <h2>Todo Items</h2>
+        {error && <h3>{error}</h3>}
+        <ul>
+          {todos?.length &&
+            todos.map((todo) => {
+              return (
+                <li key={todo?.id}>
+                  {todo?.description
+                    ? todo.description
+                    : "task should not be empty but is"}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </div>
   );
 }
